da9f52d42d36cafd0273f40eb02b294fa15c6592
TheSilentOwl <115552526+TheSilentOwl@users.noreply.github.com>
2024-08-14 08:17:16 +0300

    first python project: simple silly brand name generator

---
+++ brand_name_generator/brand_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome To This Awesome Program!")
+city = input("What city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Your brand name will be: " + city + " " + pet_name)
