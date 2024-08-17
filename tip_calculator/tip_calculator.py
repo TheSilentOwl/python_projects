dc7d0a2bb80fc4fd683dde9e8f7a86df3c1c094b
TheSilentOwl <115552526+TheSilentOwl@users.noreply.github.com>
2024-08-17 05:28:00 +0300

    simplified the split_str function a little bit

--- tip_calculator/tip_calculator.py
+++ tip_calculator/tip_calculator.py
@@ -11,29 +11,45 @@ def index_of(string, char):
         counter += 1
     return -1
 
-def split_str(string, separator):
-    splitted_str = []
-    separator_index = 0
-    while separator_index != -1:
-        separator_index = index_of(string,separator)
-        if separator_index == -1:
-            splitted_str.append(string[0::])
-            string = ""
-        else:
-            splitted_str.append(string[0: separator_index])
-            string = string[separator_index + 1::]
-    return splitted_str
+# def split_str(string, separator):
+#     splitted_str = []
+#     separator_index = 0
+#     while separator_index != -1:
+#         separator_index = index_of(string,separator)
+#         if separator_index == -1:
+#             splitted_str.append(string[0::])
+#             string = ""
+#         else:
+#             splitted_str.append(string[0: separator_index])
+#             string = string[separator_index + 1::]
+#     return splitted_str
 
 def format_num(num, decimal_num):
     numStr = str(num)
-    splittedStr = split_str(numStr, ".")
-    integer = splittedStr[0]
-    decimal = splittedStr[1]
+    splittedStrs = split_str(numStr, ".")
+    integer = splittedStrs[0]
+    decimal = splittedStrs[1]
     if len(decimal) >= 2:
         decimal = decimal[0: 2]
     else:
         decimal = decimal + "0" * (int(decimal_num) - len(decimal))
     return integer + "." + decimal
+
+def split_str(string, separator):
+    string += separator
+    splitted_strs = []
+    current_str = ""
+    counter = 0
+    while counter < len(string):
+        if string[counter] != separator:
+            current_str += string[counter]
+        else:
+            splitted_strs.append(current_str)
+            current_str = ""
+        counter += 1
+    return splitted_strs
+
+
 
 # def format_num(num, decimal_num):
 #     numStr = str(num)
