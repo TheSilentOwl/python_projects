298f301534741d14e474c9d89c3c040202d7598f
TheSilentOwl <115552526+TheSilentOwl@users.noreply.github.com>
2024-08-19 17:45:24 +0300

    initial code for pizza ordering script

---
+++ pizza_ordering_script/main.py
@@ -0,0 +1,50 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+
+bill = 0
+bill_log = []
+
+if size == "M" or size == "L":
+    if size == "M":
+        bill += 20
+        bill_log.append(str(20))
+    elif size == "L":
+        bill += 25
+        bill_log.append(str(25))
+
+
+    if pepperoni == "Y":
+        bill += 3
+        bill_log.append(str(3))
+    else:
+        bill_log.append(str(0))
+
+
+elif size == "S":
+    bill += 15
+    bill_log.append(str(15))
+
+    if pepperoni == "Y":
+        bill += 2
+        bill_log.append(str(2))
+    else:
+        bill_log.append(str(0))
+
+else:
+    print("There is no size choice as the one you type\n"
+          "only 'S', 'M' and 'L' are available")
+
+if extra_cheese == "Y":
+    bill += 1
+    bill_log.append(str(1))
+else:
+    bill_log.append(str(0))
+
+print(f"Your final bill is: (${bill}).\n"
+      f"You paid (${bill_log[0]}) for pizza size, "
+      f"(${bill_log[1]}) for pepperoni and "
+      f"(${bill_log[2]}) for extra cheese")
+
