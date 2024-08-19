12be92661a49602d2cb0838431f64ab9f907ca2f
TheSilentOwl <115552526+TheSilentOwl@users.noreply.github.com>
2024-08-19 17:59:59 +0300

    refactor the peperroni script out of the size logic to reduce code redundency

--- pizza_ordering_script/main.py
+++ pizza_ordering_script/main.py
@@ -7,35 +7,38 @@ extra_cheese = input("Do you want extra cheese? Y or N: ")
 bill = 0
 bill_log = []
 
-if size == "M" or size == "L":
-    if size == "M":
-        bill += 20
-        bill_log.append(str(20))
-    elif size == "L":
-        bill += 25
-        bill_log.append(str(25))
 
+if size == "M":
+    bill += 20
+    bill_log.append(str(20))
 
-    if pepperoni == "Y":
-        bill += 3
-        bill_log.append(str(3))
-    else:
-        bill_log.append(str(0))
-
+elif size == "L":
+    bill += 25
+    bill_log.append(str(25))
 
 elif size == "S":
     bill += 15
     bill_log.append(str(15))
 
-    if pepperoni == "Y":
-        bill += 2
-        bill_log.append(str(2))
-    else:
-        bill_log.append(str(0))
-
 else:
     print("There is no size choice as the one you type\n"
           "only 'S', 'M' and 'L' are available")
+    bill_log.append(str(0))
+
+
+
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+        bill_log.append(str(2))
+    elif size == "M" or size == "L":
+        bill += 3
+        bill_log.append(str(3))
+    else:
+        bill_log.append(str(0))
+else:
+    bill_log.append(0)
+
 
 if extra_cheese == "Y":
     bill += 1
